8b0091c653c7e6bb1b2ab3606031ae5c0b823e0d
IvanTrigueiro <ivantrigueiro-@hotmail.com>
2024-05-17 22:33:36 +0000

    3 arquivos gasolina

---
+++ gasolina.py
@@ -0,0 +1,12 @@
+# código de geração do gráfico 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+gasolina_df = pd.read_csv("/content/dataanalysis-ebac/gasolina.csv")
+
+sns.lineplot(x=gasolina_df.columns[0],
+             y=gasolina_df.columns[1],
+             data=gasolina_df.head())
+
+plt.savefig("gasolina.png")
