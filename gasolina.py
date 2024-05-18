c000621e1d282c73aa00d1003fd29b430678266f
IvanTrigueiro <ivantrigueiro-@hotmail.com>
2024-05-18 01:44:52 +0000

    comitando branch develop

--- gasolina.py
+++ gasolina.py
@@ -1,4 +1,4 @@
-# código de geração do gráfico 
+# código de geração do gráfico
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -7,6 +7,6 @@ gasolina_df = pd.read_csv("/content/dataanalysis-ebac/gasolina.csv")
 
 sns.lineplot(x=gasolina_df.columns[0],
              y=gasolina_df.columns[1],
-             data=gasolina_df.head())
+             data=gasolina_df.head()).set(title="Gasolina: Preço(R$) x Dia")
 
 plt.savefig("gasolina.png")
